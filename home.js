e832bd99dce371d11b3aeffd84d81c4687d3961f
Melyn Atieno Were <melynatieno@gmail.com>
2023-08-02 20:59:54 +0300

    Add linkedlist and node classes

---
+++ home.js
@@ -0,0 +1,25 @@
+//LINKED LISTS
+
+//Node containing data and reference to the next node
+class Node{
+    constructor(data, next){
+        this.data = data; // data that needs to be stored in the list
+        this.next = next; // address/reference to the next node in the list
+    }
+}
+
+// Class to hold our data structure(The linkedlist itself)
+class LinkedList{
+    constructor(){
+        this.head = null; // First node on the LinkedList
+        this.tail = null; // Last node on the LinkedList
+    }
+
+//Algorithmic Complexity
+
+// Add an element at the start of the list
+prepend
+}
+
+
+
